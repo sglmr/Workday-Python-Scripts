6040406966d6159aebe8a4603da0edb2eaa20c92
Stephen Gilmore <stephen>
2021-12-10 09:47:44 -0800

    i9 attachments working for list of workers

--- i-9_attachments_with_selenium.py
+++ i-9_attachments_with_selenium.py
@@ -1,6 +1,5 @@
 import os, sys
 import time
-import csv
 from tqdm import tqdm
 
 from dotenv import load_dotenv
@@ -18,12 +17,12 @@ Sample .env file:
     USERNAME=login@email.com
     PASSWORD=*password123*
     I9_TASK_URL="https://impl.workday.com/wday/authgwy/tenant/login.htmld?redirect=n"
-    TEST_WORKER="12345"
+    DOWNLOAD_FOLDER="user/Downloads"
 """
 user = os.environ.get("USERNAME")
 pw = os.environ.get("PASSWORD")
 url = os.environ.get("I9_TASK_URL")
-test_worker = os.environ.get("TEST_WORKER")
+download_folder = os.environ.get("DOWNLOAD_FOLDER")
 
 # Selenium Driver Settings
 opts = Options()
@@ -31,7 +30,7 @@ opts.set_headless(headless=False)  # View Screen
 # opts.set_headless(headless=True)  # Headless
 driver = Chrome(options=opts)
 # driver.maximize_window()
-driver.implicitly_wait(60)
+driver.implicitly_wait(10)
 
 
 # Log in to the tenant
@@ -43,46 +42,70 @@ select.send_keys(pw)
 select = driver.find_element_by_xpath("//button[@data-automation-id='goButton']")
 select.click()
 
-# Wait for page load
-driver.find_element_by_xpath("//span[contains(@title,'Workday page is loaded')]")
+worker_list = []
 
-# Enter worker to search for
-select = driver.find_element_by_id(
-    "56$202554--uid4-input"
-)  # input box to search for worker
-time.sleep(1)
-for c in test_worker:
-    select.send_keys(c)
-    time.sleep(0.2)
-select.send_keys(Keys.RETURN)
-driver.find_element_by_id("pill-247$8859")  # wait for worker selection
-driver.find_element_by_xpath("//button/span[@title='OK']").click()
+# Grab docs for every worker
+for worker in tqdm(worker_list):
 
-# Download files
-# Wait for page to load
-driver.find_element_by_xpath("//span[contains(@title,'Workday page is loaded')]")
-driver.find_element_by_id("pill-247$8859")  # wait for worker selection
+    # Wait for page load
+    driver.find_element_by_xpath("//span[contains(@title,'Workday page is loaded')]")
 
-xpath = "//div[@data-automation-id='uploadRowAttachment']/div/div"
-for doc in driver.find_elements_by_xpath(xpath):
+    # Enter worker to search for
+    try:
+        select = driver.find_element_by_id(
+            "56$202554--uid4-input"
+        )  # input box to search for worker
+        time.sleep(1)
+        for c in worker:  # Type in employee ID
+            select.send_keys(c)
+            time.sleep(0.1)
+        select.send_keys(Keys.RETURN)
+        driver.find_element_by_xpath("//div[@data-automation-id='selectedItem']")
+        driver.find_element_by_xpath("//button/span[@title='OK']").click()
+    except Exception as e:
+        tqdm.write(f"Error finding I9 for worker: {worker}.")
 
-    print(f"doc: {doc.text}")
+    # Download files
+    driver.find_element_by_xpath("//span[contains(@title,'Workday page is loaded')]")
+    doc_id = 1
+    xpath = "//div[@data-automation-id='uploadRowAttachment']/div/div"
+    try:
+        for element in driver.find_elements_by_xpath(xpath):
+            # Right click the element to get the URL to download
+            action = ActionChains(driver)
+            action.move_to_element(element).perform()  # Move to element
+            action.context_click(on_element=element).perform()  # Right click Element
+            time.sleep(1)
 
-    # Right click the element to get the URL to download
-    action = ActionChains(driver)
+            # File name & Download doc URL
+            file_name = element.text
+            copy_url = driver.find_element_by_xpath(
+                "//div[@data-automation-id='copyUrl']"
+            ).get_attribute("data-clipboard-text")
 
-    action.move_to_element(doc).perform()  # Move to element
+            # Download File
+            driver.get(copy_url)
 
-    action.context_click(on_element=doc).perform()  # Right click Element
-    time.sleep(1)
+            # Wait until file is downloaded
+            path_to_file = os.path.join(download_folder, file_name)
+            while os.path.exists(path_to_file) is False:
+                time.sleep(0.1)
 
-    copy_url = driver.find_element_by_xpath(
-        "//div[@data-automation-id='copyUrl']"
-    ).get_attribute("data-clipboard-text")
+            # Rename File
+            path_to_new_file = os.path.join(
+                download_folder, f"{worker}-{doc_id}-{file_name}"
+            )
+            os.rename(path_to_file, path_to_new_file)
 
-    print(f"url: {copy_url}")
-    # doc.click()
+            # Increment Doc ID
+            doc_id += 1
+    except Exception as e:
+        tqdm.write(f"Error downloading I9 attachments for: {worker}.")
+        tqdm.write(f"{e}")
+        continue
+
+    # Go back to the task for the next worker.
+    driver.get(url)
 
 # Exit
-time.sleep(10)  # Make sure downloads are done
 driver.quit()
