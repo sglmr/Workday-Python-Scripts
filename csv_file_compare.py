0acc38a4a8006a892b3a3ffc9f72a963d3e0de8e
Stephen <stephen@gilmore.us>
2022-11-07 13:24:12 -0800

    Update csv_file_compare.py

--- csv_file_compare.py
+++ csv_file_compare.py
@@ -1,101 +1,131 @@
-import csv, os, sys
+import csv
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
 
 # File Names (with .csv extension)
-file_1_name = "file_1.csv"
-file_2_name = "file_2.csv"
+FILES = [
+    BASE_DIR / "FILE_1.csv",
+    BASE_DIR / "FILE_2.csv",
+]
 
-results_file_name = "Compare Results.csv"
-key = "worker_id"
+KEY = "ID_FIELD"
 
-# Get Paths to files
-file_1_path = os.path.join(sys.path[0], "data_in", file_1_name)
-file_2_path = os.path.join(sys.path[0], "data_in", file_2_name)
-results_file_path = os.path.join(sys.path[0], "data_out", results_file_name)
+# Assert only comparing 2 files
+assert len(FILES) == 2
 
-d_errors = list()
+OUTFILE = BASE_DIR / "COMPARE RESULTS.csv"
 
-
-# Read in file 1:
-print("Reading file 1 to dictionary")
-file_1 = list()
-with open(file_1_path, mode="r", encoding="utf-8-sig") as csvfile:
-    reader = csv.DictReader(csvfile, dialect="excel")
-    for row in reader:
-        file_1.append(dict(row))
-file_1_columns = list(file_1[0].keys())
+ERRORS = list()
 
 
-# Read in file 2:
-print("Reading file 2 to dictionary")
-file_2 = list()
-with open(file_2_path, mode="r", encoding="utf-8-sig") as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        file_2.append(dict(row))
-file_2_columns = list(file_2[0].keys())
+class CSVFile:
+    def __init__(self, file_path: Path, key: str):
+        self.key = key
+
+        assert file_path.suffix == ".csv"
+        self.file_path = file_path
+
+        # Read file into data_rows
+        self.data_rows = dict()
+        with open(self.file_path, mode="r", encoding="utf-8-sig") as f:
+            reader = csv.DictReader(f, dialect="excel")
+            for count, row in enumerate(reader):
+                row_key = row[self.key]
+                self.data_rows[row_key] = row
+
+                if count == 0:
+                    # Use the first data row to get the column names
+                    self.col_names = [f"{x}" for x in row.keys()]
+
+                    assert self.key in self.col_names  # assert the key is in the file
+                    assert len(self.col_names) == len(
+                        set(self.col_names)
+                    )  # Assert no duplicate column names
+
+    def __str__(self):
+        return f"{self.file_path.stem}"
 
 
-# Get a distinct list of the columns to compare that match in both files
-columns = list(set(file_2_columns) & set(file_1_columns))
-columns.sort()
-columns.remove(key)
+# Create the CSVFile objects for each file
+csv1 = CSVFile(file_path=FILES[0], key=KEY)
+csv2 = CSVFile(file_path=FILES[1], key=KEY)
 
-# Compare File 1 to File 2
-print("Comparing File 1 to File 2...")
-for f1_row in file_1:
-    for f2_row in file_2:
-        if (
-            f1_row[key] != f2_row[key]
-        ):  # Cycle through data keys until employee is found
-            continue
-        for col in columns:
-            if f1_row[col] == f2_row[col]:
+
+# Check for records in file 1, but not file 2
+for k in csv1.data_rows.keys():
+    found_record = csv2.data_rows.get(k, None)
+    if found_record:
+        for field in csv1.col_names:
+            csv1_data = csv1.data_rows[k][field]
+            csv2_data = csv2.data_rows[k].get(field, None)
+
+            if csv1_data == csv2_data:
                 continue
+            elif csv1_data != csv2_data:
+                if csv2_data:
+                    note = "data mismatch"
+                elif not csv2_data:
+                    note = "missing field"
+
+                ERRORS.append(
+                    {
+                        "key": k,
+                        "col": field,
+                        "f1": f"{csv1_data or ''}",
+                        "f2": f"{csv2_data or ''}",
+                        "notes": note,
+                    }
+                )
+    else:
+        ERRORS.append(
+            {
+                "key": k,
+                "col": "",
+                "f1": "",
+                "f2": "",
+                "notes": f"Record missing from {csv2}",
+            }
+        )
+
+# Check for records in file 1, but not file 2
+for k in csv2.data_rows.keys():
+    found_record = csv1.data_rows.get(k, None)
+    if found_record:
+        for field in csv2.col_names:
+            csv1_data = csv1.data_rows[k].get(field, None)
+            csv2_data = csv2.data_rows[k][field]
+
+            if csv1_data is None:
+                ERRORS.append(
+                    {
+                        "key": k,
+                        "col": field,
+                        "f1": f"{csv1_data or ''}",
+                        "f2": f"{csv2_data or ''}",
+                        "notes": "missing field",
+                    }
+                )
             else:
-                d = dict()
-                d[key] = f1_row[key]
-                d["column"] = col
-                d["f1_value"] = f1_row[col]
-                d["f2_value"] = f2_row[col]
-                d_errors.append(d)
-        break
+                continue
 
-
-# Find Workers in File 1, missing from File 2
-for f1_row in file_1:
-    match_test = 0
-    for f2_row in file_2:
-        if f1_row[key] == f2_row[key]:
-            match_test = 1
-            break
-    if match_test != 1:
-        d = dict()
-        d[key] = f1_row[key]
-        d["column"] = key
-        d["f1_value"] = f1_row[key]
-        d["f2_value"] = ""
-        d_errors.append(d)
-
-# Find Workers in File 2, missing from File 1
-for f2_row in file_2:
-    match_test = 0
-    for f1_row in file_1:
-        if f1_row[key] == f2_row[key]:
-            match_test = 1
-            break
-    if match_test != 1:
-        d = dict()
-        d[key] = f2_row[key]
-        d["column"] = key
-        d["f1_value"] = ""
-        d["f2_value"] = f2_row[key]
-        d_errors.append(d)
-
+    else:
+        ERRORS.append(
+            {
+                "key": k,
+                "col": "",
+                "f1": "",
+                "f2": "",
+                "notes": f"Record missing from {csv1}",
+            }
+        )
 
 # Write the results to a csv file
-with open(results_file_path, "w") as csvfile:
-    fieldnames = [key, "column", "f1_value", "f2_value"]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+with open(OUTFILE, "w") as csvfile:
 
-    writer.writeheader()
-    writer.writerows(d_errors)
+    writer = csv.writer(csvfile)
+    writer.writerow([f"{KEY}", "Field", f"{csv1}", f"{csv2}", "Notes"])
+
+    for err in ERRORS:
+        writer.writerow([err["key"], err["col"], err["f1"], err["f2"], err["notes"]])
